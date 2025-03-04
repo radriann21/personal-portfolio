fbd264006d8eb65ef096cc23b900ea899bc60608
Adrian Rangel <adrianrangelargu@gmail.com>
2025-03-03 20:45:28 -0400

    fix: changed to npm fix: fixed colors and contrast

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel"
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
