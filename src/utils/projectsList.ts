cd38ea52733067bc593356a83b8065d07c8c50b5
Adrian Rangel <adrianrangelargu@gmail.com>
2025-03-26 16:49:34 -0400

    feat: refactor code and improved UI

--- src/utils/projectsList.ts
+++ src/utils/projectsList.ts
@@ -1,23 +1,37 @@
 export const projects = [
   {
-    title: "Nextionary",
-    description: "App web que permite buscar palabras, revisar sus significados y guardarlas/eliminarlas.",
-    technologies: ["NextJS", "TypeScript", "Chakra-UI", "Zustand"],
-    link: "https://nextionary.vercel.app/",
-    code: "https://github.com/radriann21/nextionary"
+    title: "Space Tourism Landing",
+    description: "Una landing page para una agencia relacionada al turismo espacial, usando NextJS junto a Tailwind, asegurando un diseño enfocado en el dinamismo.",
+    technologies: ["NextJS", "TypeScript", "motion"],
+    link: "https://space-tourism-eight-jade.vercel.app/",
+    code: "https://github.com/radriann21/space-tourism-nextjs"
   },
   {
-    title: "ArkanoidJS",
-    description: "Juego de arkanoid hecho en JavaScript, usando Canvas para dibujar y POO para lógica.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    link: "https://radriann21.github.io/javascript-arkanoid/",
-    code: "https://github.com/radriann21/arkanoidjs"
+    title: "Sunnyside Agency Landing",
+    description: "Landing page enfocada a una página de una empresa dedicada al diseño de marca y la creación de campañas de marketing digital. Desarrollada usando Astro.",
+    technologies: ["Astro", "TailwindCSS", "daisyui"],
+    link: "https://github.com/radriann21/sunnyside-agency-landing-astro",
+    code: "https://sunnyside-agency-landing-astro.vercel.app/"
   },
   {
-    title: "Insure Landing Page",
-    description: "Landing page para una agencia de seguros usando Astro y Tailwind para los estilos.",
-    technologies: ["Astro", "TailwindCSS"],
-    link: "https://insure-landing-page-astro.vercel.app/",
-    code: "https://github.com/radriann21/insure-landing-page-astro"
+    title: "PrettyAIColors",
+    description: "Aplicación web para la creación de paletas de colores mediante diferentes algoritmos de generación de colores, además de inteligencia artificial. Permite además guardar paletas de colores para su uso futuro y exportarlas.",
+    technologies: ["NextJS", "TailwindCSS", "Zustand", "Gemini API", "chroma-js"],
+    link: "https://prettyai-colors.vercel.app/",
+    code: "https://github.com/radriann21/prettyai-colors"
+  },
+  {
+    title: "Readmydoc",
+    description: "Aplicación web que permite la generación y edición de documentos markdown mediante plantillas enfocadas a diferentes tipos de proyectos de código abierto.",
+    technologies: ["NextJS", "TailwindCSS", "Monaco Editor", "Zustand", "React Markdown"],
+    link: "https://readmydoc.vercel.app/",
+    code: "https://github.com/radriann21/readmydoc"
+  },
+  {
+    title: "E-Commerce - Origin Shop",
+    description: "Aplicación web de un E-Commerce para la compra de productos enfocada en la experiencia de usuario, interfaz agradable y facilidad de uso.",
+    technologies: ["NextJS", "TailwindCSS", "Zustand", "Supabase"],
+    link: "#",
+    code: "#"
   }
 ]
